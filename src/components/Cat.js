f6fd67811ca3df48085f69215377d67387a272c1
William Chu <Williamchu51@gmail.com>
2018-09-27 09:34:11 -0700

    add glyph and logic to display origin detail for breed

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -7,6 +7,12 @@ export default function Cat(props) {
     <div className="cat-container">
       <div className="cat-portrait-bg">
         <img src={catImg} className="cat-portrait" alt="image of cat" />
+        {(props.catDetail.origin.toLowerCase() === "crossbreed" || props.catDetail.origin.toLowerCase() === 'mutation') &&
+          <div className="glyph-bg">
+            {props.catDetail.origin.toLowerCase() === "mutation" && <i className="fal fa-dna"></i>}
+            {props.catDetail.origin.toLowerCase() === "crossbreed" && <i className="fas fa-adjust"></i>}
+          </div>
+        }
       </div>
       <h2>{props.catDetail.breed}</h2>
       <p>{props.catDetail.country}</p>
