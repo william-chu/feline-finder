26879587d54770ddb3b1ef848fa897cab0acc5ce
William Chu <Williamchu51@gmail.com>
2018-09-27 08:04:45 -0700

    add display loop and styling to Cat and CatList

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Cat.css';
+import catImg from '../assets/img/cat.png';
 
-export default function Cat() {
+export default function Cat(props) {
   return (
     <div className="cat-container">
-      <p>single cat</p>
+      <div className="cat-portrait-bg">
+        <img src={catImg} className="cat-portrait" alt="image of cat" />
+      </div>
+      <h2>{props.catDetail.breed}</h2>
+      <p>{props.catDetail.country}</p>
     </div>
   )
 }
--- src/components/CatList.js
+++ src/components/CatList.js
@@ -5,8 +5,12 @@ import Cat from './Cat';
 export default function CatList(props) {
   return (
     <div className="catlist-container">
-      <p>component works</p>
-      <Cat />
+      <h1>Your Results:</h1>
+      <div className="catlist-grid">
+        {props.catResults.map(function(obj,i) {
+          return <Cat catDetail={obj} key={i} />;
+        })}
+      </div>
     </div>
   )
 }
